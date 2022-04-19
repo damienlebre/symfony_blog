f458754ab38a43a8724eec77e467b7337a3be19b
J7 <J7DevWeb@gmail.com>
2022-04-19 10:59:05 +0200

    CRUD tag

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -2,19 +2,90 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Exception;
+use App\Entity\Tag;
+use App\Form\TagType;
+use App\Repository\TagRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TagController extends AbstractController
 {
     /**
-     * @Route("/tag", name="app_tag")
+     * @Route("/badges", name="tag_list")
      */
-    public function index(): Response
+    public function index(TagRepository $repo): Response
     {
-        return $this->render('tag/index.html.twig', [
-            'controller_name' => 'TagController',
+        $tags = $repo->findAll();
+
+        return $this->render('tag/list.html.twig', [
+            'tags' => $tags
         ]);
+    }
+
+    /**
+     * @Route("/creation-de-badges", name="tag_new")
+     */
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+       $tag = new Tag();
+       $form = $this->createForm(TagType::class, $tag); 
+
+       $form->handleRequest($request);
+
+       if($form->isSubmitted() && $form->isValid())
+       {
+           $em->persist($tag);
+
+           try{
+               $em->flush();
+           }catch(Exception $e){
+               return $this->redirectToRoute('tag_new');
+           }
+
+           return $this->redirectToRoute('tag_list');
+       }
+
+        return $this->render('tag/new.html.twig', [
+            'form' => $form->createView() 
+        ]);
+    }
+
+    /**
+     * @Route("/modifier-badge/{id}", name="tag_edit")
+     */
+    public function edit(Request $request, Tag $tag, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(TagType::class, $tag);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            try{
+                $em->flush();
+            }catch(Exception $e){
+                return $this->redirectToRoute('tag_edit');
+            }
+
+            return $this->redirectToRoute('tag_list');
+        }
+
+        return $this->render('tag/edit.html.twig', [
+            'form' => $form->createView() 
+        ]);
+    }
+
+        /**
+     * @Route("/supprimer-badge/{id}", name="tag_delete")
+     */
+    public function delete(Tag $tag, EntityManagerInterface $em): Response
+    {
+        $em->remove($tag);
+        $em->flush();
+        
+        return $this->redirectToRoute("tag_list");
     }
 }
